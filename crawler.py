36c04eeb53376b70f30c7a39f38df0615de4ed82
CG-AA <syankuan@gmail.com>
2024-02-19 23:00:36 +0800

    clean and rewrite decode.py

--- crawler.py
+++ crawler.py
@@ -1,50 +1,45 @@
-from selenium import webdriver
 import json
-from selenium.webdriver.common.by import By
 import os
 from time import sleep
 
-def init():
-    sett = initsetting()
-    driver = initdriver(sett)
-    return sett, driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-def initsetting():
+def load_settings():
     try:
         with open('setting.json', 'r') as file:
-            settings = json.load(file)
-        return settings
+            return json.load(file)
     except FileNotFoundError:
         raise FileNotFoundError("File not found: setting.json")
 
-def initdriver(sett):
-    driver = webdriver.Chrome()
-    return driver
+def init_driver():
+    return webdriver.Chrome()
 
-def login(sett, driver, serial):
+def login(driver, settings, serial):
     driver.get('https://apcs.camp/vip')
-    driver.find_element(By.NAME, "email").send_keys(sett['login_settings']['acc'+str(serial)])
-    driver.find_element(By.NAME, "password").send_keys(sett['login_settings']['pas'+str(serial)])
+    driver.find_element(By.NAME, "email").send_keys(settings['login_settings']['acc'+str(serial)])
+    driver.find_element(By.NAME, "password").send_keys(settings['login_settings']['pas'+str(serial)])
     driver.find_element(By.ID, ":r2:").click()
     sleep(1)
     driver.get('https://apcs.camp/vip/judge')
     sleep(1)
 
-def getNwrite(driver, i):
-    driver.get("https://judge.apcs.camp/problems/" + str(i))
-    with open("./problems/problem" + str(i) + ".html", "w") as file:
+def write_problem(driver, problem_id):
+    driver.get(f"https://judge.apcs.camp/problems/{problem_id}")
+    with open(f"./problems/problem{problem_id}.html", "w", encoding='utf-8') as file:
         file.write(driver.page_source)
 
-def getproblemsHTML(driver, sett):
+def download_problems(driver, settings):
     os.makedirs("./problems", exist_ok=True)
-    for i in sett['catch']['a2']:
-        for j in range(int(sett['catch']['a2'][i][0]), int(sett['catch']['a2'][i][1])+1):
-            getNwrite(driver, j)
-
+    for i in settings['catch'][f'a{settings["temp"]}']:
+        for j in range(int(settings['catch'][f'a{settings["temp"]}'][i][0]), int(settings['catch'][f'a{settings["temp"]}'][i][1])+1):
+            write_problem(driver, j)
 
 def main():
-    sett, driver = init()
-    login(sett, driver, 2)
-    getproblemsHTML(driver, sett)
+    settings = load_settings()
+    driver = init_driver()
+    login(driver, settings, settings['temp'])
+    download_problems(driver, settings)
 
-main()
+if __name__ == "__main__":
+    main()
